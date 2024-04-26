31f25617689b281da8921e7ed0c41f889e3e1c49
mattdevtx <matt@mattrodriguez.tech>
2024-04-26 14:15:32 -0500

    adds scroll event to window

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 'use strict'
 
 const dropdownBtn = document.querySelector('.dropdown')
-
 const navMenu = document.querySelector('.nav-menu')
-
 const navMenuExit = document.querySelector('.nav-menu-exit')
+const navbar = document.querySelector('.navbar-fixed')
+
+window.addEventListener('scroll', function () {
+	if (window.scrollY > 100) {
+		navbar.classList.add('navbar-color-bg')
+	} else {
+		navbar.classList.remove('navbar-color-bg')
+	}
+})
 
 dropdownBtn.addEventListener('click', () => {
-  navMenu.classList.toggle('show-nav-menu')
+	navMenu.classList.toggle('show-nav-menu')
 })
 
 navMenuExit.addEventListener('click', () => {
-  navMenu.classList.toggle('show-nav-menu')
+	navMenu.classList.toggle('show-nav-menu')
 })
