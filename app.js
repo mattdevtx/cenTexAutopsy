78030dc8f6fef7e11c23352f312ca94165577e4b
mattdevtx <matt@mattrodriguez.tech>
2024-04-26 11:11:00 -0500

    adds functionality to nav-menu-exit 'button'

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 'use strict'
+
+const dropdownBtn = document.querySelector('.dropdown')
+
+const navMenu = document.querySelector('.nav-menu')
+
+const navMenuExit = document.querySelector('.nav-menu-exit')
+
+dropdownBtn.addEventListener('click', () => {
+  navMenu.classList.toggle('show-nav-menu')
+})
+
+navMenuExit.addEventListener('click', () => {
+  navMenu.classList.toggle('show-nav-menu')
+})
